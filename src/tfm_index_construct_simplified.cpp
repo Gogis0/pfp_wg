b762b146a96ff98e7d93594663d058cacc1817fd
andy <andrejbalaz001@gmail.com>
2021-08-30 15:30:00 +0200

    add: custom wt_blcd

--- src/tfm_index_construct_simplified.cpp
+++ src/tfm_index_construct_simplified.cpp
@@ -31,6 +31,8 @@ void print_tfm(const tfm_index<> &tfm) {
     // cout << tfm.C << endl;              // C[1..|A|]
 }
 
+wt_blcd<> construct_from_vector(initializer_list<uint32_t> il);
+
 int main() {
     construct_config::byte_algo_sa = LIBDIVSUFSORT;
     cache_config config = cache_config(false, "../data/", "tmp");
@@ -47,7 +49,8 @@ int main() {
     print_tfm(tfm);
 
     tfm_index<> tfm2;
-    wt_blcd<> wt = tfm.L;
+    // wt_blcd<> wt = tfm.L;
+    wt_blcd<> wt = construct_from_vector({4, 0, 3, 3, 3, 1, 2, 3});
     // int_vector_buffer<>& buf = int_vector_buffer<>()
     // wt_blcd_int<> = wt_blcd_int();
     bit_vector dout = int_vector<1>({1, 1, 0, 0, 0, 1, 1, 1, 1});
@@ -60,4 +63,58 @@ int main() {
     store_to_file(tfm, "../data/yeast.tunnel");
     store_to_file(tfm2, "../data/yeast.tunnel2");
     return 0;
+}
+
+wt_blcd<> construct_from_vector(initializer_list<uint32_t> il) {
+    wt_blcd<> wt;
+    const uint8_t size = wt_blcd<>::alphabet_category::WIDTH;
+    int_vector<size> text = int_vector<size>(il);
+
+    string tmp_file_name = "tmp";
+    store_to_file(text, tmp_file_name);
+
+    int_vector_buffer<size> text_buf(tmp_file_name);
+    wt_blcd<> tmp(text_buf, text_buf.size());
+    wt.swap(tmp);
+    remove(tmp_file_name);
+    return wt;
+}
+
+wt_huff<> construct_wt() {
+    string file = "@testinput.iv8";
+    wt_huff<> wt;
+    uint8_t num_bytes = 1;
+    // construct(wt, file, num_bytes);
+    // void construct(t_index& idx, std::string file, uint8_t num_bytes=0)
+    tMSS file_map;
+    cache_config config;
+    if (is_ram_file(file)) {
+        config.dir = "@";
+    }
+    // construct(wt, file, config, num_bytes);
+    // void construct(t_index& idx, const std::string& file, cache_config& config, uint8_t num_bytes=0)
+    typename wt_huff<>::index_category index_tag;
+    // construct(wt, file, config, num_bytes, index_tag);
+    // void construct(t_index& idx, const std::string& file, cache_config& config, uint8_t num_bytes, wt_tag)
+    auto event = memory_monitor::event("construct wavelet tree");
+    if ((wt_huff<>::alphabet_category::WIDTH==8 and num_bytes <= 1)
+        or (wt_huff<>::alphabet_category::WIDTH==0 and num_bytes != 'd')) {
+        int_vector_buffer<wt_huff<>::alphabet_category::WIDTH> text_buf(file, std::ios::in, 1024*1024, num_bytes*8, (bool)num_bytes);
+        wt_huff<> tmp(text_buf, text_buf.size());
+        wt.swap(tmp);
+    } else {
+        int_vector<wt_huff<>::alphabet_category::WIDTH> text;
+        load_vector_from_file(text, file, num_bytes);
+        std::string tmp_key = util::to_string(util::pid())+"_"+util::to_string(util::id());
+        std::string tmp_file_name = cache_file_name(tmp_key, config);
+        store_to_file(text, tmp_file_name);
+        util::clear(text);
+        {
+            int_vector_buffer<wt_huff<>::alphabet_category::WIDTH> text_buf(tmp_file_name);
+            wt_huff<> tmp(text_buf, text_buf.size());
+            wt.swap(tmp);
+        }
+        sdsl::remove(tmp_file_name);
+    }
+    return wt;
 }

f6fb164ced3d93815cdbb043c0ce24275df03497
andy <andrejbalaz001@gmail.com>
2021-08-30 17:00:03 +0200

    add: found out the alphabet size

--- src/tfm_index_construct_simplified.cpp
+++ src/tfm_index_construct_simplified.cpp
@@ -23,12 +23,10 @@ void print_tfm(const tfm_index<> &tfm) {
     cout << endl;
     cout << tfm.dout << endl;           // O[1..|v|]
     cout << tfm.din << endl;            // I[1..|v|]
-    cout << tfm.L.size() <<  endl;
-    for (uint i = 0; i < tfm.L.size(); i++) {   // How to get alphabet size?
-        cout << tfm.C[i] <<  " " ;
+    for (uint i = 0; i <= tfm.L.sigma; i++) {
+        cout << tfm.C[i] <<  " " ;      // C[1..|A|]
     }
-    cout << endl;
-    // cout << tfm.C << endl;              // C[1..|A|]
+    cout << endl << endl;
 }
 
 wt_blcd<> construct_from_vector(initializer_list<uint32_t> il);
@@ -39,26 +37,23 @@ int main() {
 
     tfm_index<> tfm;
     // auto parse = init_parse({1, 2, 3, 1, 2, 3, 4});
-    auto parse = init_parse({1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 4});
-    // cout << util::to_string(parse) << endl;
-    // cout << to_string(parse.width()) << endl;
-    // cout << parse << endl;
+    // auto parse = init_parse({1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 4});
+    auto parse = init_parse({1, 2, 1, 2});
     csa_wt<wt_blcd_int<>> csa;
     construct_im(csa, parse);
     construct_tfm_index(tfm, move(csa), config);
     print_tfm(tfm);
 
+
+
     tfm_index<> tfm2;
-    // wt_blcd<> wt = tfm.L;
     wt_blcd<> wt = construct_from_vector({4, 0, 3, 3, 3, 1, 2, 3});
-    // int_vector_buffer<>& buf = int_vector_buffer<>()
-    // wt_blcd_int<> = wt_blcd_int();
     bit_vector dout = int_vector<1>({1, 1, 0, 0, 0, 1, 1, 1, 1});
     bit_vector din  = int_vector<1>({1, 1, 1, 1, 0, 0, 0, 1, 1});
     uint64_t text_len = tfm.size();
 
     construct_tfm_index_tmp(tfm2, text_len, move(wt), move(dout), move(din));
-    print_tfm(tfm2);
+    // print_tfm(tfm2);
 
     store_to_file(tfm, "../data/yeast.tunnel");
     store_to_file(tfm2, "../data/yeast.tunnel2");
@@ -77,44 +72,5 @@ wt_blcd<> construct_from_vector(initializer_list<uint32_t> il) {
     wt_blcd<> tmp(text_buf, text_buf.size());
     wt.swap(tmp);
     remove(tmp_file_name);
-    return wt;
-}
-
-wt_huff<> construct_wt() {
-    string file = "@testinput.iv8";
-    wt_huff<> wt;
-    uint8_t num_bytes = 1;
-    // construct(wt, file, num_bytes);
-    // void construct(t_index& idx, std::string file, uint8_t num_bytes=0)
-    tMSS file_map;
-    cache_config config;
-    if (is_ram_file(file)) {
-        config.dir = "@";
-    }
-    // construct(wt, file, config, num_bytes);
-    // void construct(t_index& idx, const std::string& file, cache_config& config, uint8_t num_bytes=0)
-    typename wt_huff<>::index_category index_tag;
-    // construct(wt, file, config, num_bytes, index_tag);
-    // void construct(t_index& idx, const std::string& file, cache_config& config, uint8_t num_bytes, wt_tag)
-    auto event = memory_monitor::event("construct wavelet tree");
-    if ((wt_huff<>::alphabet_category::WIDTH==8 and num_bytes <= 1)
-        or (wt_huff<>::alphabet_category::WIDTH==0 and num_bytes != 'd')) {
-        int_vector_buffer<wt_huff<>::alphabet_category::WIDTH> text_buf(file, std::ios::in, 1024*1024, num_bytes*8, (bool)num_bytes);
-        wt_huff<> tmp(text_buf, text_buf.size());
-        wt.swap(tmp);
-    } else {
-        int_vector<wt_huff<>::alphabet_category::WIDTH> text;
-        load_vector_from_file(text, file, num_bytes);
-        std::string tmp_key = util::to_string(util::pid())+"_"+util::to_string(util::id());
-        std::string tmp_file_name = cache_file_name(tmp_key, config);
-        store_to_file(text, tmp_file_name);
-        util::clear(text);
-        {
-            int_vector_buffer<wt_huff<>::alphabet_category::WIDTH> text_buf(tmp_file_name);
-            wt_huff<> tmp(text_buf, text_buf.size());
-            wt.swap(tmp);
-        }
-        sdsl::remove(tmp_file_name);
-    }
     return wt;
 }

e45b612c2213c9c58403d3d2a967aaa1f0f13cd7
andy <andrejbalaz001@gmail.com>
2021-08-31 16:59:26 +0200

    add: printable tunneled tfm

--- src/tfm_index_construct_simplified.cpp
+++ src/tfm_index_construct_simplified.cpp
@@ -17,16 +17,86 @@ int_vector<> init_parse(initializer_list<uint32_t> il) {
 }
 
 void print_tfm(const tfm_index<> &tfm) {
+    cout << "L:\t";
     for (unsigned char i : tfm.L) {
-        cout << to_string(i) << " ";    // L[1..|e|]
+        cout << to_string(i) << " ";                // L[1..|e|]
     }
     cout << endl;
-    cout << tfm.dout << endl;           // O[1..|v|]
-    cout << tfm.din << endl;            // I[1..|v|]
+    cout << "O:\t" << tfm.dout << endl;             // O[1..|v|]
+    cout << "I:\t" << tfm.din << endl;              // I[1..|v|]
+    cout << "C:\t";
     for (uint i = 0; i <= tfm.L.sigma; i++) {
-        cout << tfm.C[i] <<  " " ;      // C[1..|A|]
+        cout << tfm.C[i] <<  " " ;                  // C[1..|A|]
     }
     cout << endl << endl;
+}
+
+ulong get_value(const tfm_index<> &tfm, ulong u) {
+    return tfm.L.inverse_select(u).second;
+}
+
+void my_backwardstep(const tfm_index<> &tfm, pair<ulong, ulong> &pos, ulong &value) {
+    ulong &in_node = pos.first;
+    ulong &tunnel_num = pos.second;
+    value = get_value(tfm, in_node);
+    in_node = tfm.L.inverse_select(in_node).first + tfm.C[value];
+    ulong rank = tfm.din_rank(in_node + 1);
+    if (tfm.din[in_node] == 0) { tunnel_num = in_node - tfm.din_select(rank); }
+    in_node = tfm.dout_select(rank);
+    if (tfm.dout[in_node + 1] == 0) { in_node += tunnel_num; tunnel_num = 0; }
+}
+
+// next step: read BWT-Tunneling/seqana/include/dbg_algorithms.hpp
+
+class edge{
+public:
+    ulong u;    // in node
+    ulong v;    // out node
+    ulong l;    // label
+    ulong t;    // tunnel number
+
+    edge() = default;
+    edge(uint in_node, uint out_node, uint label, uint tunnel){
+        u = in_node;
+        v = out_node;
+        l = label;
+        t = tunnel;
+    }
+    edge get_next(const tfm_index<> &tfm) const {
+        auto L = tfm.L;
+        auto O = tfm.dout;
+        auto I = tfm.din;
+        auto C = tfm.C;
+        auto rI = tfm.din_rank;
+        auto sI = tfm.din_select;
+        auto sO = tfm.dout_select;
+
+        edge next = edge();
+        next.u = v;
+        next.t = t;
+        next.l = L.inverse_select(next.u).second;
+        next.v = L.inverse_select(next.u).first + C[next.l];
+        if (I[next.v] == 0) {
+            next.t = next.v - sI(rI(next.v + 1));
+        }
+        next.v = sO(rI(next.v + 1));
+        if (O[next.v + 1] == 0) {
+            next.v += next.t; next.t = 0;
+        }
+        return next;
+    }
+    void print() const {
+        cout << "Tunnel " << t << " from " << u << " to " << v << " with label " << l << "." << endl;
+    }
+};
+
+
+void print_original(const tfm_index<> &tfm) {
+    edge e = edge(0, 0, 0, 0);
+    for (auto i = tfm.size(); i > 0; i--) {
+        e = e.get_next(tfm);
+        e.print();
+    }
 }
 
 wt_blcd<> construct_from_vector(initializer_list<uint32_t> il);
@@ -39,10 +109,12 @@ int main() {
     // auto parse = init_parse({1, 2, 3, 1, 2, 3, 4});
     // auto parse = init_parse({1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 4});
     auto parse = init_parse({1, 2, 1, 2});
+    // auto parse = init_parse({1, 2, 3, 1, 2, 3});
     csa_wt<wt_blcd_int<>> csa;
     construct_im(csa, parse);
     construct_tfm_index(tfm, move(csa), config);
     print_tfm(tfm);
+    print_original(tfm);
 
 
 

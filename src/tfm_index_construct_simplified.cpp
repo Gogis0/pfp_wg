8216cb522dbd51d598fb7a65e1381c1bd4f81bfe
andy <andrejbalaz001@gmail.com>
2021-09-02 17:21:32 +0200

    add: unparse test works

--- src/tfm_index_construct_simplified.cpp
+++ src/tfm_index_construct_simplified.cpp
@@ -3,10 +3,19 @@
 int main() {
     tfm_index<> tfm;
     // auto parse = init_parse({1, 2, 1, 2});
-    auto parse = init_parse({2, 1, 4, 5, 3, 2, 1, 4, 5});
+    auto parse = init_parse({1, 2, 1, 3, 1, 2, 1, 3, 1});
     my_construct(tfm, parse);
     print_tfm(tfm);
     print_original(tfm);
+
+    tfm_index<> tfm2;
+    wt_blcd<> wt = construct_from_vector({2, 2, 0, 1, 1, 0, 0, 0});
+    bit_vector dout = int_vector<1>({1, 1, 1, 1, 0, 1, 1, 1, 1});
+    bit_vector din  = int_vector<1>({1, 1, 1, 1, 1, 1, 1, 0, 1});
+    uint64_t text_len = 9;
+    construct_tfm_index_tmp(tfm2, text_len, move(wt), move(dout), move(din));
+    print_tfm(tfm2);
+    print_original(tfm2);
 
     return 0;
 }

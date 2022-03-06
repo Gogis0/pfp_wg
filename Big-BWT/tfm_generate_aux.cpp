41b6e412389e8970ecbe81ea8bd65b2ae99e92c8
Gogis0 <adriangoga0@gmail.com>
2022-03-06 23:31:34 +0100

    WIP: restructure, move everything to BigBWT

--- Big-BWT/tfm_generate_aux.cpp
+++ Big-BWT/tfm_generate_aux.cpp
@@ -28,6 +28,9 @@
 #include <vector>
 
 #include "tfm_index.hpp"
+extern "C" {
+#include "utils.h"
+}
 
 using namespace std;
 using namespace sdsl;
@@ -35,48 +38,58 @@ using namespace sdsl;
 typedef typename sdsl::int_vector<>::size_type size_type;
 
 void printUsage( char **argv ) {
-	cerr << "USAGE: " << argv[0] << " TFMFILE OCCFILE" << endl;
+	cerr << "USAGE: " << argv[0] << "w TFMFILE OCCFILE" << endl;
 	cerr << "TFMFILE:" << endl;
 	cerr << "  File where to store the serialized trie" << endl;
 };
 
 int main(int argc, char **argv) {
 	//check parameters
-	if (argc != 3) {
+	if (argc < 3) {
 		printUsage( argv );
 		cerr << "At least 2 parameters expected" << endl;
 		return 1;
 	}
-	
+	// read dictionary file
+        FILE *g = open_aux_file(argv[2],EXTDICT,"rb");
+        fseek(g,0,SEEK_END);
+        long dsize = ftell(g);
+        if(dsize<0) die("ftell dictionary");
+        if(dsize<=1+4) die("invalid dictionary file");
+        cout  << "Dictionary file size: " << dsize << endl;
+        #if !M64
+        if(dsize > 0x7FFFFFFE) {
+            printf("Dictionary size greater than  2^31-2!\n");
+            printf("Please use 64 bit version\n");
+            exit(1);
+        }
+        #endif
+
+        uint8_t *d = new uint8_t[dsize];
+        rewind(g);
+        long e = fread(d,1,dsize,g);
+        if(e!=dsize) die("fread");
+        fclose(g);
+
 	//load tunneled fm index
 	tfm_index<> tfm;
 	load_from_file( tfm, argv[1] );
-
-        uint32_t dphrases = 0;
-	for (size_type i = 0; i < tfm.L.size(); i++) {
-            uint32_t x = (uint32_t)tfm.L[i];
-            if (x > dphrases) dphrases = x;
-        }
-
         vector<vector<uint32_t>> phrase_sources(tfm.L.size());
-        vector<uint32_t> phrase_occs(dphrases, (uint32_t)0);
+        vector<uint32_t> phrase_occs(dsize, 0);
 
 	auto p = tfm.end();
         int last = 1;
-	for (size_type i = 0; i < tfm.L.size(); i++) {
+	for (size_type i = 1; i < tfm.L.size(); i++) {
             //if (tfm.dout[i] == 1) last = i;
             auto c = (uint)tfm.L[i];
             //phrase_sources[last-1].push_back((uint)tfm.L[i]);
             phrase_occs[c]++;
             //cout << (uint)tfm.L[i] << " ";
 	}
-        // read the dictionary
-        FILE *fdict = fopen(argv[2], "r");
-        for (uint32_t i )
 
         // write the numbers of occurrences for each phrase
         FILE *focc = fopen(argv[3], "wb");
-        for (uint32_t i = 0; i < dphrases; i++) {
+        for (uint32_t i = 0; i < dsize; i++) {
             size_t s = fwrite(&phrase_occs[i], sizeof(phrase_occs[i]), 1, focc);
             if (s != 1) {
                 cout << "Error writing to OCC file" << endl;
@@ -91,4 +104,5 @@ int main(int argc, char **argv) {
 	    cout << S[i] << ' ';
 	}
         */
+        delete d;
 }
--- Big-BWT/tfm_index_invert.cpp
+++ Big-BWT/tfm_index_invert.cpp
@@ -35,16 +35,16 @@ using namespace sdsl;
 typedef typename sdsl::int_vector<>::size_type size_type;
 
 void printUsage( char **argv ) {
-	cerr << "USAGE: " << argv[0] << " TFMFILE OCCFILE" << endl;
+	cerr << "USAGE: " << argv[0] << " TFMFILE" << endl;
 	cerr << "TFMFILE:" << endl;
 	cerr << "  File where to store the serialized trie" << endl;
 };
 
 int main(int argc, char **argv) {
 	//check parameters
-	if (argc != 3) {
+	if (argc != 2) {
 		printUsage( argv );
-		cerr << "At least 2 parameters expected" << endl;
+		cerr << "At least 1 parameter expected" << endl;
 		return 1;
 	}
 	
@@ -52,43 +52,14 @@ int main(int argc, char **argv) {
 	tfm_index<> tfm;
 	load_from_file( tfm, argv[1] );
 
-        uint32_t dphrases = 0;
-	for (size_type i = 0; i < tfm.L.size(); i++) {
-            uint32_t x = (uint32_t)tfm.L[i];
-            if (x > dphrases) dphrases = x;
-        }
-
-        vector<vector<uint32_t>> phrase_sources(tfm.L.size());
-        vector<uint32_t> phrase_occs(dphrases, (uint32_t)0);
-
+	uint *S = new uint[tfm.size()];
+	S[tfm.size()-1] = 0;
 	auto p = tfm.end();
-        int last = 1;
-	for (size_type i = 0; i < tfm.L.size(); i++) {
-            //if (tfm.dout[i] == 1) last = i;
-            auto c = (uint)tfm.L[i];
-            //phrase_sources[last-1].push_back((uint)tfm.L[i]);
-            phrase_occs[c]++;
-            //cout << (uint)tfm.L[i] << " ";
+	for (size_type i = 1; i < tfm.size(); i++) {
+	S[tfm.size() - i - 1] = (uint)tfm.backwardstep( p );
 	}
-        // read the dictionary
-        FILE *fdict = fopen(argv[2], "r");
-        for (uint32_t i )
-
-        // write the numbers of occurrences for each phrase
-        FILE *focc = fopen(argv[3], "wb");
-        for (uint32_t i = 0; i < dphrases; i++) {
-            size_t s = fwrite(&phrase_occs[i], sizeof(phrase_occs[i]), 1, focc);
-            if (s != 1) {
-                cout << "Error writing to OCC file" << endl;
-                exit(1);
-            }
-        }
-        cout << "OCC file successfully written!" << endl;
-
-        // write 
-        /*
 	for (size_type i=0; i<tfm.size()-1; i++) {
 	    cout << S[i] << ' ';
 	}
-        */
+	cout << endl;
 }

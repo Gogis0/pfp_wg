32f359d1838c42523e1302355109b0e590ba1c12
Gogis0 <adriangoga0@gmail.com>
2022-03-21 00:01:07 +0100

    Finally the auxiliary data generation is done

--- Big-BWT/tfm_generate_aux.cpp
+++ Big-BWT/tfm_generate_aux.cpp
@@ -1,26 +1,3 @@
-/*
- * tfm_index_invert.cpp for Edge minimization in de Bruijn graphs
- * Copyright (c) 2019 Uwe Baier, Pascal Weber All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #include <iostream>
 #include <map>
 #include <deque>
@@ -46,9 +23,9 @@ struct Dict {
 };
 
 void printUsage( char **argv ) {
-	cerr << "USAGE: " << argv[0] << "w TFMFILE OCCFILE" << endl;
-	cerr << "TFMFILE:" << endl;
-	cerr << "  File where to store the serialized trie" << endl;
+	cerr << "USAGE: " << argv[0] << "w BASENAME" << endl;
+	cerr << "BASENAME:" << endl;
+	cerr << "  Name prefix of the data files to process." << endl;
 };
 
 Dict read_dictionary(char *filename) {
@@ -88,80 +65,131 @@ Dict read_dictionary(char *filename) {
         return res;
 }
 
+void write_occs(char *basename, Dict dict, vector<vector<uint32_t>> &sources) {
+    char *name;
+    asprintf(&name, "%s.%s", basename, "occ");
+    FILE *focc = fopen(name, "wb");
+    for (int i = 0; i < dict.dwords; i++) {
+        uint32_t cnt = sources[i].size();
+        size_t s = fwrite(&cnt, sizeof(uint32_t), 1, focc);
+        if (s != 1) {
+            cout << "Error writing to OCC file" << endl;
+            exit(1);
+        }
+    }
+    cout << "OCC file successfully written!" << endl;
+    fclose(focc);
+}
+
+void write_ilist(char *basename, Dict dict, vector<vector<uint32_t>> &sources) {
+    char *name;
+    asprintf(&name, "%s.%s", basename, "ilist");
+    FILE *filist = fopen(name, "wb");
+    for (int i = 0; i < dict.dwords; i++) {
+        for (int j = 0; j < (int)sources[i].size(); j++) {
+            size_t s = fwrite(&sources[i][j], sizeof(uint32_t), 1, filist);
+            if (s != 1) {
+                cout << "Error writing to ILIST file" << endl;
+                exit(1);
+            }
+        }
+    }
+    cout << "ILIST file successfully written!" << endl;
+    fclose(filist);
+}
+
 int main(int argc, char **argv) {
 	//check parameters
-	if (argc < 1) {
+	if (argc < 2) {
 		printUsage( argv );
 		cerr << "At least 2 parameters expected" << endl;
 		return 1;
 	}
         int w = atoi(argv[1]);
+        // load the dictionary and the tunneled WG
         struct Dict dict = read_dictionary(argv[2]);
-
-	//load tunneled fm index
         char *name;
         asprintf(&name, "%s.%s", argv[2], "tunnel");
-	tfm_index<> tfm;
-	load_from_file(tfm, name);
-        vector<vector<uint8_t>> phrase_outlabels(dict.dwords+1); // the list of ourgoing edges for each vertex
+        tfm_index<> tfm;
+        load_from_file(tfm, name);
+        cout << "TFM loaded" << endl;
+
         vector<vector<uint32_t>> phrase_sources(dict.dwords+1); // the list of ourgoing edges for each vertex
+        vector<vector<uint32_t>> phrase_destinations(dict.dwords+1); // the list of ourgoing edges for each vertex
+
+        for (int i = 0; i < tfm.L.size(); i++) {
+            uint32_t act_char = tfm.L[i];
+            phrase_sources[act_char].push_back(i);
+        }
+        cout << "Inverted lists created!" << endl;
+
+        // write the numbers of occurrences for each phrase
+        write_occs(argv[2], dict, phrase_sources);
+        // write the concatenation of the 'inverted lists' of the phrases
+        write_ilist(argv[2], dict, phrase_sources);
+
         vector<vector<bool>> phrase_din(dict.dwords+1);
         vector<vector<bool>> phrase_dout(dict.dwords+1);
+        vector<vector<uint8_t>> phrase_outlabels(dict.dwords+1); // the list of ourgoing edges for each vertex
+        uint64_t last_size = 0, bitvector_size = 0;
+        for (int c = 0; c < dict.dwords+1; c++) {
+            vector<bool> seen(tfm.dout_rank(tfm.L.size()), false); // mark processed vertices
+            for (int i = 0; i < (int)phrase_sources[c].size(); i++) {
 
-        int node = 0;
-        for (int i = 0; i < tfm.L.size(); i++) {
-            phrase_sources[tfm.L[i]].push_back(node);
-            phrase_dout[tfm.L[i]] = (tfm.dout[i] == 1);
-            if (tfm.dout[i+1] == 1) node++;
-        }
-        vector<map<uint32_t, uint32_t>> H(dict.dwords+1);
-        // remap phrase_sources so that 
-        for (int i = 0; i < dict.dwords; i++) {
-            for (int j = 0; j < phrase_sources[i]; j++) {
-                H[i][phrase_sources[j]] = j;
-            }
-        }
+                uint32_t pos = phrase_sources[c][i];
+                auto is = tfm.L.inverse_select(pos).first; // inverse_select returns rank(L[i], i), L[i])
+                uint32_t new_i = tfm.C[c] + is; // we want to include the is-th position into the rank
+                uint32_t din_rank = tfm.din_rank(new_i + 1);
+                if (!seen[din_rank]) {
+                    phrase_din[c].push_back(1);
+                    phrase_dout[c].push_back(1);
+                    seen[din_rank] = true;
 
-        // circumvent the TFM and create the necessary data
-        auto p = tfm.end();
-        uint32_t c = -1, last = 0;
-	for (size_type i = 0; i < tfm.size()-1; i++) {
-            c = (uint32_t)tfm.backwardstep(p);
-            if (c == 0) phrase_outlabels[last].push_back('$');
-            else phrase_outlabels[last].push_back(dict.d[dict.end[c-1]-w-1]);
-            last = c;
-	}
-        phrase_outlabels[last].push_back(dict.d[dict.end[c-1]-w-1]);
-
-        // write the numbers of occurrences for each phrase
-        asprintf(&name, "%s.%s", argv[2], "occ");
-        FILE *focc = fopen(name, "wb");
-        for (int i = 0; i < dict.dwords; i++) {
-            uint32_t cnt = phrase_sources[i].size();
-            size_t s = fwrite(&cnt, sizeof(uint32_t), 1, focc);
-            if (s != 1) {
-                cout << "Error writing to OCC file" << endl;
-                exit(1);
-            }
-        }
-        cout << "OCC file successfully written!" << endl;
-
-        // write the numbers of occurrences for each phrase
-        asprintf(&name, "%s.%s", argv[2], "out");
-        FILE *fsrc = fopen(name, "wb");
-        for (int i = 0; i < phrase_outlabels.size(); i++) {
-            cout << i << " : ";
-            for (int j = 0; j < phrase_outlabels[i].size(); j++) {
-                cout << (char)phrase_outlabels[i][j] << ' ';
-                size_t s = fwrite(&phrase_outlabels[i], sizeof(phrase_outlabels[i]), 1, fsrc);
-                if (s != 1) {
-                    cout << "Error writing to SOURCES file" << endl;
-                    exit(1);
+                    // process all the outgoing edges
+                    int act_i = new_i;
+                    while (1) {
+                        uint32_t act_phrase = tfm.L[act_i];
+                        uint8_t chartowrite = (act_phrase == 0) ? '$' : dict.d[dict.end[act_phrase-1]-w-1];
+                        phrase_outlabels[c].push_back(chartowrite);
+                        if (tfm.dout[++act_i] == 0) phrase_dout[c].push_back(0);
+                        else break;
+                    }
+                } else {
+                    // many incoming edges
+                    new_i = tfm.dout_select(din_rank);
+                    uint32_t act_phrase = tfm.L[new_i];
+                    uint8_t chartowrite = (act_phrase == 0) ? '$' : dict.d[dict.end[act_phrase-1]-w-1];
+                    phrase_outlabels[c].push_back(chartowrite);
+                    phrase_din[c].push_back(0);
                 }
             }
-            cout << endl;
+            last_size += phrase_outlabels[c].size();
+            bitvector_size += phrase_din[c].size();
+            phrase_din[c].push_back(1);
+            phrase_dout[c].push_back(1);
         }
-        cout << "SOURCES file successfully written!" << endl;
+        cout << "Phrase outlabels created!" << endl;
 
-        delete dict.d;
+        // prepare for serialization
+        int_vector<> din (bitvector_size, 0, 1);
+        int_vector<> dout (bitvector_size, 0, 1);
+        int_vector<> L (last_size, 0, 8);
+        uint64_t cnt_din = 0, cnt_dout = 0, cnt_L = 0;
+        for (int i = 0; i < dict.dwords+1; i++) {
+            cout << "phrase: " << i+1 << endl;
+            for (int j = 0; j < phrase_outlabels[i].size(); j++) {
+                cout << phrase_outlabels[i][j];
+            } cout << endl;
+            for (int j = 0; j < phrase_outlabels[i].size(); j++)    L[cnt_L++]          = phrase_outlabels[i][j];
+            for (int j = 0; j < phrase_din[i].size(); j++)          din[cnt_din++]      = phrase_din[i][j]; 
+            for (int j = 0; j < phrase_dout[i].size(); j++)         dout[cnt_dout++]    = phrase_dout[i][j];
+        }
+
+        asprintf(&name, "%s.%s", argv[2], "last");
+        store_to_file(L, string (name));
+        asprintf(&name, "%s.%s", argv[2], "din");
+        store_to_file(din, string(name));
+        asprintf(&name, "%s.%s", argv[2], "dout");
+        store_to_file(dout, string(name));
+        cout << "SOURCES file successfully written!" << endl;
 }
